55643cbc6029c9450c23e49b3352bab5b9d687a9
MinhTuLeHoang <minhtulehoang1302@gmail.com>
2026-05-31 15:08:39 +0700

    cleanup(general): remove all legacy general helper shell scripts

--- src/generalScripts/emit-shell-command.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Description: Utility to emit shell commands to the parent shell via integration pipe.
-
-emit_shell_command() {
-    if [ -n "$MINHTHETUS_SHELL_PIPE" ]; then
-        echo "$1" >> "$MINHTHETUS_SHELL_PIPE"
-    fi
-}
