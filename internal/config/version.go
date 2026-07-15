c5f2a1b1a724e58f63a952513c3a49a3a0c6177b
MinhTuLeHoang <minhtulehoang1302@gmail.com>
2026-07-15 17:14:10 +0700

    [bump version] v1.5.2

--- internal/config/version.go
+++ internal/config/version.go
@@ -2,7 +2,7 @@ package config
 
 const (
 	// Version is the current stable version of minhthetus-cli
-	Version = "1.5.1"
+	Version = "1.5.2"
 	// BuildDate is the release date of the current stable version
 	BuildDate = "2026-07-15"
 )
